7d303ec4f6ba9ec040fe9a926e79011f0f8744f8
mmulahvictor <mmulahvictor@gmail.com>
2022-09-05 18:52:59 +0300

    add records in my db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,4 +3,20 @@ puts "🌱 Seeding spices..."
 # Seed your database here
 
 
+# farmers instances
+farmer1 = Farmer.create(name: "Victor Mmulah")
+farmer2 = Farmer.create(name: "Mark Lumbasi")
+farmer3 = Farmer.create(name: "Philip John")
+
+# customer instances
+customer1 = Customer.create(name: "John Smith")
+customer2 = Customer.create(name: "Mary Wanjiku")
+customer3 = Customer.create(name: "Wastalas Kiprop")
+
+
+# goods instances
+item1 = Goods.create(name: "carrots", amount: 20, farmer_id: farmer1, customer_id: customer3)
+item2 = Goods.create(name: "beans", amount: 10, farmer_id: farmer2, customer_id: customer1)
+item3 = Goods.create(name: "maize", amount: 6, farmer_id: farmer3, customer_id: customer2)
+
 puts "✅ Done seeding!"
