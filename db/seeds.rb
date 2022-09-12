0cb6053c89aa6eacacd08543c4efbfa2e7decefd
mmulahvictor <mmulahvictor@gmail.com>
2022-09-12 03:00:30 +0300

    minor changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,7 +13,7 @@ Customer.create(name: "Wastalas Kiprop")
 
 
 # goods instances
-Item.create(name: "carrots", image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSZ4vbALMASziiuRwFujXrITo_nHVYg6HjWQ&usqp=CAU", amount: 20, farmer_id: 1 customer_id: 1)
+Item.create(name: "carrots", image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQSZ4vbALMASziiuRwFujXrITo_nHVYg6HjWQ&usqp=CAU", amount: 20, farmer_id: 1, customer_id: 1)
 
 
 puts "✅ Done seeding!"
