ad34b55b6027262d7e997e7a67b5b8bd9dbc84f2
mmulahvictor <mmulahvictor@gmail.com>
2022-09-12 03:03:03 +0300

    minor changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,9 +2,9 @@ puts "🌱 Seeding spices..."
 
 # Seed your database here
 
-Farmer.create(name: "Victor Mmulah", phone: 254720345673, location: "Kakamega")
-Farmer.create(name: "Mark Lumbasi", phone: 254720344756, location: "Nairobi")
-Farmer.create(name: "Philip John", phone: 254720342933, location: "Nyeri")
+Farmer.create(name: "Victor Mmulah", phone: 720345673, location: "Kakamega")
+Farmer.create(name: "Mark Lumbasi", phone: 720344756, location: "Nairobi")
+Farmer.create(name: "Philip John", phone: 720342933, location: "Nyeri")
 
 # customer instances
 Customer.create(name: "John Smith")
